3b37d13f0d9bf6035943595167e2b88176740de3
albnzhum <albnzhum@gmail.com>
2023-02-07 04:26:59 +0500

    splitList

--- emae/src/App.java
+++ emae/src/App.java
@@ -1,5 +1,37 @@
+import java.util.*;
+
+
 public class App {
+    static int randomInt;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Integer> source = new ArrayList<Integer>();
+        ArrayList<Integer> positive = new ArrayList<Integer>();
+        ArrayList<Integer> negative = new ArrayList<Integer>();
+        splitList(source, positive, negative);
+    }
+
+    static void splitList(ArrayList<Integer> source, ArrayList positive, ArrayList negative)
+    {
+        source = new ArrayList<Integer>();
+        Random rand = new Random(); //заполнение случайными числа
+        for (int i = 0; i < 10; i++)
+        {
+            randomInt = (int)(Math.random()*(200+1)) - 100;
+            source.add(randomInt);
+        }
+
+        positive = new ArrayList<>();
+        negative = new ArrayList<>();
+        final ArrayList<Integer> negativeFinal = negative;//ошибку выдавало без final
+        final ArrayList<Integer> positiveFinal = positive;
+
+        source.stream().forEach(i -> (i < 0 ? negativeFinal : positiveFinal).add(i));
+        Collections.sort(positive);
+        Collections.sort(negative);
+        
+        System.out.println("Source as-is: " + source);
+        System.out.println("Positive: " + positive);
+        System.out.println("Negative: " + negative);
+
     }
 }
